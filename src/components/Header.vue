57fe68f17113b96690a7934c45afb7688d59dae7
Krizaldiw <khalifrizaldi04@gmail.com>
2022-11-03 14:02:55 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <nav class="navbar navbar-light bg-light">
+      <span class="navbar-brand mb-0 h1 ">
+        RewFood</span>
+      <span class="navbar-brand mb-0 h1">
+        <!-- Create<span class="badge badge-secondary"></span>
+        Login<span class="badge badge-secondary"></span>
+        Register<span class="badge badge-secondary"></span> -->
+        Items <span class="badge badge-secondary">{{ totalQty }}</span>
+      </span>
+    </nav>
+  </header>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Header",
+  computed: {
+    ...mapGetters(["cart"]),
+    totalQty() {
+      return this.cart.reduce((a, b) => a + b.qty, 0);
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  margin-bottom: 10vh;
+}
+</style>
---
+++ src/components/Shop.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row">
+    <div
+      class="card"
+      style="width: 15rem;"
+      v-for="item in products"
+      :key="item.id"
+    >
+      <img :src="item.imgUrl" class="card-img-top" :alt="item.title" />
+      <div class="card-body">
+        <p class="card-title">{{ item.title }}</p>
+        <p class="card-text">$ {{ item.price.toLocaleString() }}</p>
+        <button class="btn btn-warning" @click="addItemToCart(item)">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  name: "Shop",
+  mounted() {
+    this.getProducts();
+  },
+  computed: {
+    ...mapGetters(["products"]),
+  },
+  methods: {
+    ...mapActions(["addItemToCart", "getProducts"]),
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  padding: 20px;
+  margin: 5px;
+  border: 1;
+}
+</style>
